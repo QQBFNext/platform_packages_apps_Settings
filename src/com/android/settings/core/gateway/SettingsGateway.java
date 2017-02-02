198a49080d109da3221e587e8b48d25d6fa2c59d
ezio84 <brabus84@gmail.com>
2017-02-03 00:26:51 +0100

    Allow to quickly open Running Services from meminfo view
    @Jackeagle: Updated for A14
    
    Change-Id: I2d3f4d2c6592c0d7776c7b5d629b189d2a73c96b
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -44,6 +44,7 @@ import com.android.settings.accounts.ManagedProfileSettings;
 import com.android.settings.applications.AppDashboardFragment;
 import com.android.settings.applications.ProcessStatsSummary;
 import com.android.settings.applications.ProcessStatsUi;
+import com.android.settings.applications.RunningServices;
 import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.appcompat.UserAspectRatioDetails;
 import com.android.settings.applications.appinfo.AlarmsAndRemindersDetails;
@@ -392,7 +393,8 @@ public class SettingsGateway {
             ScreenTimeoutSettings.class.getName(),
             ResetNetwork.class.getName(),
             VibrationIntensitySettingsFragment.class.getName(),
-            Blissify.class.getName()
+            Blissify.class.getName(),
+	    RunningServices.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -440,5 +442,6 @@ public class SettingsGateway {
             Settings.MemtagPageActivity.class.getName(),
             Settings.NavigationModeSettingsActivity.class.getName(),
             Settings.BlissifySettingsActivity.class.getName(),
+            Settings.DevRunningServicesActivity.class.getName()
     };
 }
