TheParasiteProject/packages_apps_Settings

Settings: ButtonSettings: Move to SwitchPreferenceCompat
Change-Id: I7889969a751fef7eb6cf1d911687df57815802b4

src/com/android/settings/custom/buttons/ButtonSettings.java
@@ -35,7 +35,7 @@ import android.provider.Settings;
 import android.util.Log;
 
 import androidx.fragment.app.DialogFragment;
-import androidx.preference.SwitchPreference;
+import androidx.preference.SwitchPreferenceCompat;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
@@ -96,11 +96,11 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     private ListPreference mAssistLongPressAction;
     private ListPreference mAppSwitchPressAction;
     private ListPreference mAppSwitchLongPressAction;
-    private SwitchPreference mCameraWakeScreen;
-    private SwitchPreference mCameraSleepOnRelease;
-    private SwitchPreference mCameraLaunch;
-    private SwitchPreference mDisableNavigationKeys;
-    private SwitchPreference mSwapCapacitiveKeys;
+    private SwitchPreferenceCompat mCameraWakeScreen;
+    private SwitchPreferenceCompat mCameraSleepOnRelease;
+    private SwitchPreferenceCompat mCameraLaunch;
+    private SwitchPreferenceCompat mDisableNavigationKeys;
+    private SwitchPreferenceCompat mSwapCapacitiveKeys;
 
     private Handler mHandler;
 
@@ -124,16 +124,16 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     private static final String CATEGORY_NAVBAR = "navbar_key";
     private static final String CATEGORY_POWER = "power_key";
 
-    private SwitchPreference mNavigationMenuArrowKeys;
-    private SwitchPreference mNavigationInverse;
+    private SwitchPreferenceCompat mNavigationMenuArrowKeys;
+    private SwitchPreferenceCompat mNavigationInverse;
     private Preference mNavigationGestures;
-    private SwitchPreference mNavigationCompactLayout;
+    private SwitchPreferenceCompat mNavigationCompactLayout;
     private Preference mTorchLongPressPower;
-    private SwitchPreference mPowerEndCall;
-    private SwitchPreference mHomeAnswerCall;
+    private SwitchPreferenceCompat mPowerEndCall;
+    private SwitchPreferenceCompat mHomeAnswerCall;
     private ListPreference mVolumeKeyCursorControl;
-    private SwitchPreference mVolumeWakeScreen;
-    private SwitchPreference mVolumeMusicControls;
+    private SwitchPreferenceCompat mVolumeWakeScreen;
+    private SwitchPreferenceCompat mVolumeMusicControls;
     private PreferenceCategory mNavbarCategory;
     private Preference mGesturePowerMenu;
     

QQBFNext/platform_packages_apps_Settings

Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

src/com/android/settings/bliss/buttons/ButtonSettings.java
@@ -44,7 +44,7 @@ import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceManager;
 import androidx.preference.PreferenceScreen;
-import androidx.preference.SwitchPreference;
+import androidx.preference.SwitchPreferenceCompat;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
@@ -132,26 +132,26 @@ public class ButtonSettings extends SettingsPreferenceFragment
     private ListPreference mAssistLongPressAction;
     private ListPreference mAppSwitchPressAction;
     private ListPreference mAppSwitchLongPressAction;
-    private SwitchPreference mCameraWakeScreen;
-    private SwitchPreference mCameraSleepOnRelease;
-    private SwitchPreference mCameraLaunch;
+    private SwitchPreferenceCompat mCameraWakeScreen;
+    private SwitchPreferenceCompat mCameraSleepOnRelease;
+    private SwitchPreferenceCompat mCameraLaunch;
     private ListPreference mVolumeKeyCursorControl;
-    private SwitchPreference mVolumeWakeScreen;
-    private SwitchPreference mVolumeMusicControls;
-    private SwitchPreference mSwapVolumeButtons;
-    private SwitchPreference mVolumePanelOnLeft;
-    private SwitchPreference mDisableNavigationKeys;
-    private SwitchPreference mNavigationArrowKeys;
+    private SwitchPreferenceCompat mVolumeWakeScreen;
+    private SwitchPreferenceCompat mVolumeMusicControls;
+    private SwitchPreferenceCompat mSwapVolumeButtons;
+    private SwitchPreferenceCompat mVolumePanelOnLeft;
+    private SwitchPreferenceCompat mDisableNavigationKeys;
+    private SwitchPreferenceCompat mNavigationArrowKeys;
     private ListPreference mNavigationBackLongPressAction;
     private ListPreference mNavigationHomeLongPressAction;
     private ListPreference mNavigationHomeDoubleTapAction;
     private ListPreference mNavigationAppSwitchLongPressAction;
     private ListPreference mEdgeLongSwipeAction;
-    private SwitchPreference mPowerEndCall;
-    private SwitchPreference mHomeAnswerCall;
-    private SwitchPreference mTorchLongPressPowerGesture;
+    private SwitchPreferenceCompat mPowerEndCall;
+    private SwitchPreferenceCompat mHomeAnswerCall;
+    private SwitchPreferenceCompat mTorchLongPressPowerGesture;
     private ListPreference mTorchLongPressPowerTimeout;
-    private SwitchPreference mSwapCapacitiveKeys;
+    private SwitchPreferenceCompat mSwapCapacitiveKeys;
 
     private PreferenceCategory mNavigationPreferencesCat;
 